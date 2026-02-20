37f75282141279a8a7c9b872dc120f4fef566ba5
Peter Kral <peter.kral@pdfix.net>
2026-02-20 17:03:13 +0100

    ci(action,PVQ-4861): Unite update_version.sh script.

--- update_version.sh
+++ update_version.sh
@@ -36,7 +36,7 @@ sed -i.bak "s|v0\.0\.0|${version}|g" config.json && rm config.json.bak
 echo "Replaced all occurrences of 'v0.0.0' with '${version}' in config.json."
 
 # Replace "latest" with the provided argument in config.json
-sed -i.bak "s/:latest/:${version}/g" config.json && rm config.json.bak
+sed -i.bak "s|:latest|:${version}|g" config.json && rm config.json.bak
 
 echo "Replaced all occurrences of 'latest' with '${version}' in config.json."
 
@@ -49,6 +49,6 @@ if [ ! -f "README.md" ]; then
 fi
 
 # Replace "latest" with the provided argument in README.md
-sed -i.bak "s/:latest/:${version}/g" README.md && rm README.md.bak
+sed -i.bak "s|:latest|:${version}|g" README.md && rm README.md.bak
 
 echo "Replaced all occurrences of 'latest' with '${version}' in README.md."
